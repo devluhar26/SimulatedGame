8ddf59df5c8f8d3b15e80bd81d80e75c395c3235
dev <devluhar26@gmail.com>
2024-07-05 12:28:41 +0100

    .

--- user_terminal/pages/1_new strategy.py
+++ user_terminal/pages/1_new strategy.py
@@ -1,4 +1,5 @@
 import streamlit as st
+
 @st.experimental_dialog("Cast your vote")
 def vote(item):
     st.write(f"Why is {item} your favorite?")
@@ -6,3 +7,12 @@ def vote(item):
     if st.button("Submit"):
         st.session_state.vote = {"item": item, "reason": reason}
         st.rerun()
+
+if "vote" not in st.session_state:
+    st.write("Vote for your favorite")
+    if st.button("A"):
+        vote("A")
+    if st.button("B"):
+        vote("B")
+else:
+    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
