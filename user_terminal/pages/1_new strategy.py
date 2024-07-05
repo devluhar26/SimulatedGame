60dd511f07995bbcd74992ba1a7d4cd55365936d
dev <devluhar26@gmail.com>
2024-07-05 13:11:05 +0100

    .

--- user_terminal/pages/1_new strategy.py
+++ user_terminal/pages/1_new strategy.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
 @st.experimental_dialog("Cast your vote")
-def vote(item):
+def logic(item):
     st.write(f"Why is {item} your favorite?")
     reason = st.text_input("Because...")
     if st.button("Submit"):
-        st.session_state.vote = {"item": item, "reason": reason}
+        st.session_state.logic = {"item": item,}
         st.rerun()
 
-if "vote" not in st.session_state:
+if "logic" not in st.session_state:
     st.title("Create a new trading strategy here")
     name = st.text_input("enter bot name here")
     if st.button("impliment"):
-        vote(name)
+        logic(name)
 
 else:
-    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+    f"You voted for {st.session_state.logic['item']} because "
 
