ad09c786d6781a2bddddf9a9c1801c6aebbd5ae6
dev <devluhar26@gmail.com>
2024-07-05 15:00:08 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("simulated trading game")
-st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+st.write("need to import table [docs.streamlit.io](https://docs.streamlit.io/).")
