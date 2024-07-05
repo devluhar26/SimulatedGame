741ca6f5ab1c52e528290c605974a3fd0aac7d60
dev <devluhar26@gmail.com>
2024-07-05 15:34:43 +0100

    .

--- user_terminal/pages/1_new strategy.py
+++ user_terminal/pages/1_new strategy.py
@@ -17,3 +17,16 @@ if "logic" not in st.session_state:
 else:
     f"{st.session_state.logic['name']} has now been added (this is when the strat are added to sql)"
 
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
