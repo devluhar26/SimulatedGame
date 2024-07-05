af4bed5f13d90a9931217ce89eb935a0da7f44d5
dev <devluhar26@gmail.com>
2024-07-05 11:23:12 +0100

    .

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("live trading game")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
