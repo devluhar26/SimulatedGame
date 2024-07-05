06f16667957726ffc1c345a6f48ca1b0b28976e0
dev <devluhar26@gmail.com>
2024-07-05 12:02:50 +0100

    .

--- user terminal/main.py
+++ user terminal/main.py
@@ -1,13 +1,2 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
-
-st.write("# Welcome to Streamlit! 👋")
-
-st.sidebar.success("Select a demo above.")
-
-st.markdown(
-    "Streamlit is an open-source app framework built specifically for")
