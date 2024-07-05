ea6cef7a86fb8660369f6c09c352ace851942b4a
dev <devluhar26@gmail.com>
2024-07-05 15:10:24 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -24,10 +24,3 @@ event = st.dataframe(
     selection_mode='single-row'
 )
 
-if len(event.selection['rows']):
-    selected_row = event.selection['rows'][0]
-    country = df.iloc[selected_row]['Country']
-    capital = df.iloc[selected_row]['Capital']
-
-    st.session_state['country_data'] = {'country': country, 'capital': capital}
-    st.page_link('pages/detail.py', label=f'Goto {country} Page', icon='🗺️')
