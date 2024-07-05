a56aa855b9c689807a92e0d9ad8a64cb06cc114c
dev <devluhar26@gmail.com>
2024-07-05 15:01:37 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("simulated trading game")
 st.write("need to import table [docs.streamlit.io](https://docs.streamlit.io/).")
+
+
+df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+
+st.dataframe(df)  # Same as st.write(df)
