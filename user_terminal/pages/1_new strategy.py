eb81372f14c17b238736ee2fc48eff6d0a11238b
dev <devluhar26@gmail.com>
2024-07-05 15:36:02 +0100

    .

--- user_terminal/pages/1_new strategy.py
+++ user_terminal/pages/1_new strategy.py
@@ -1,27 +1,27 @@
 import streamlit as st
 
-@st.experimental_dialog("Cast your vote")
-def logic(name):
-    st.write(f"set the trading logic for {name}")
-    ##add bot logic widgets here
-    if st.button("add"):
-        st.session_state.logic = {"name": name,}
-        st.rerun()
 
-if "logic" not in st.session_state:
-    st.title("Create a new trading strategy here")
-    name = st.text_input("enter bot name here")
-    if st.button("impliment"):
-        logic(name)
 
-else:
-    f"{st.session_state.logic['name']} has now been added (this is when the strat are added to sql)"
-
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+tab1, tab2, tab3 = st.tabs(["view", "new", "modify"])
 
 with tab1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    @st.experimental_dialog("Cast your vote")
+    def logic(name):
+        st.write(f"set the trading logic for {name}")
+        ##add bot logic widgets here
+        if st.button("add"):
+            st.session_state.logic = {"name": name, }
+            st.rerun()
+
+
+    if "logic" not in st.session_state:
+        st.title("Create a new trading strategy here")
+        name = st.text_input("enter bot name here")
+        if st.button("impliment"):
+            logic(name)
+
+    else:
+        f"{st.session_state.logic['name']} has now been added (this is when the strat are added to sql)"
 
 with tab2:
    st.header("A dog")
