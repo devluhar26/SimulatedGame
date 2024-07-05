cd445fda74c861a5ceb66030d15b358a63df808b
dev <devluhar26@gmail.com>
2024-07-05 12:57:11 +0100

    .

--- user_terminal/pages/1_new strategy.py
+++ user_terminal/pages/1_new strategy.py
@@ -3,16 +3,17 @@ import streamlit as st
 @st.experimental_dialog("bot logic")
 def logic(name):
     st.write(f"select the frequency and logic for {name}")
-    reason = st.text_input("Because...")
+    ## add widgets to set logic and frequency here
     if st.button("add"):
         st.rerun()
+if "logic" not in st.session_state:
 
-st.title("Create a new trading strategy here")
-name=st.text_input("enter bot name here")
-if st.button("impliment"):
-    logic(name)
+    st.title("Create a new trading strategy here")
+    name=st.text_input("enter bot name here")
+    if st.button("impliment"):
+        logic(name)
 
-
-
+else:
+    f"{st.session_state.logic["name"]} has been added"
 
 
