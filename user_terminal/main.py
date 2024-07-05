4de429f8a7bb38915e613c721bf79a65684d223f
dev <devluhar26@gmail.com>
2024-07-05 15:09:30 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -5,7 +5,29 @@ import numpy as np
 st.title("simulated trading game")
 st.write("need to import table [docs.streamlit.io](https://docs.streamlit.io/).")
 
+import streamlit as st
+import pandas as pd
 
-df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
 
-st.dataframe(df)  # Same as st.write(df)
+data = {
+    'Country': ['United States', 'Canada', 'Germany', 'France', 'Japan'],
+    'Capital': ['Washington, D.C.', 'Ottawa', 'Berlin', 'Paris', 'Tokyo']
+}
+
+
+df = pd.DataFrame(data)
+st.title('Countries and Their Capitals')
+
+event = st.dataframe(
+    df,
+    on_select='rerun',
+    selection_mode='single-row'
+)
+
+if len(event.selection['rows']):
+    selected_row = event.selection['rows'][0]
+    country = df.iloc[selected_row]['Country']
+    capital = df.iloc[selected_row]['Capital']
+
+    st.session_state['country_data'] = {'country': country, 'capital': capital}
+    st.page_link('pages/detail.py', label=f'Goto {country} Page', icon='🗺️')
