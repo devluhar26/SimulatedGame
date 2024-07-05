029b271d99cbf3f884475e9b775667ee1e797782
dev <devluhar26@gmail.com>
2024-07-05 12:26:18 +0100

    .

--- user_terminal/pages/1_new strategy.py
+++ user_terminal/pages/1_new strategy.py
@@ -1,1 +1,8 @@
 import streamlit as st
+@st.experimental_dialog("Cast your vote")
+def vote(item):
+    st.write(f"Why is {item} your favorite?")
+    reason = st.text_input("Because...")
+    if st.button("Submit"):
+        st.session_state.vote = {"item": item, "reason": reason}
+        st.rerun()
