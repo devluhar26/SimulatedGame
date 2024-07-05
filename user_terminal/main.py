4310905c2b4c44958248b9be601a607baacbc9db
dev <devluhar26@gmail.com>
2024-07-05 15:19:11 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import random
 st.title("simulated trading game")
 st.write("need to import table [docs.streamlit.io](https://docs.streamlit.io/).")
 
@@ -10,17 +10,16 @@ import pandas as pd
 
 
 data = {
-    'Country': ['United States', 'Canada', 'Germany', 'France', 'Japan'],
-    'Capital': ['Washington, D.C.', 'Ottawa', 'Berlin', 'Paris', 'Tokyo']
+    'strategy name': ['United States', 'Canada', 'Germany', 'France', 'Japan'],
+    'performance':  [[random.randint(0, 5000) for _ in range(30)] for _ in range(5)],
 }
 
 
 df = pd.DataFrame(data)
-st.title('Countries and Their Capitals')
-
 event = st.dataframe(
     df,
     on_select='rerun',
-    selection_mode='single-row'
+    selection_mode='single-row',
+    column_config={"performance":st.column_config.LineChartColumn("performance", y_min=0, y_max=5000)},
 )
 
