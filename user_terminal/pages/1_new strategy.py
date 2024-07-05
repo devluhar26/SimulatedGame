020793f45ea036443565298189472d77f2473c3f
dev <devluhar26@gmail.com>
2024-07-05 12:39:19 +0100

    .

--- user_terminal/pages/1_new strategy.py
+++ user_terminal/pages/1_new strategy.py
@@ -3,6 +3,7 @@ import streamlit as st
 @st.experimental_dialog("Cast your vote")
 def vote():
     st.write(f"Why is your favorite?")
+    reason = st.text_input("Because...")
     if st.button("Submit"):
         pass
 if __name__ == "__main__":
