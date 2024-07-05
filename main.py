668d9e2348d1c6c97ed7c26effa03d6939cd8eb3
dev <devluhar26@gmail.com>
2024-07-05 11:42:18 +0100

    .

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ st.title("simulated trading game")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
