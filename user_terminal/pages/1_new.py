ccfa238824b742533ee72b2f10fee1a0b0b5f8b3
dev <devluhar26@gmail.com>
2024-07-08 10:29:38 +0100

    .

--- user_terminal/pages/1_new.py
+++ user_terminal/pages/1_new.py
@@ -9,18 +9,19 @@ def logic(name):
     st.write(f"set the trading logic for {name}")
     ##add bot logic widgets here
     if st.button("add"):
+        st.session_state.logic = {"name": name, }
         st.rerun()
-        st.session_state.name = name
-        ok(name)
-@st.experimental_dialog(f'{st.session_state.name} has now been added (this is when the strat are added to sql)')
-def ok(name):
-    pass
 
 
-st.title("Create a new trading strategy here")
-name = st.text_input("enter bot name here")
-response_dict = code_editor("\n\n\n\n\n\n\n\n\n\n")
-if st.button("implement"):
-    logic(name)
+if "logic" not in st.session_state:
+    st.title("Create a new trading strategy here")
+    name = st.text_input("enter bot name here")
+    response_dict = code_editor("\n\n\n\n\n\n\n\n\n\n")
 
+    if st.button("impliment"):
+        logic(name)
 
+else:
+    f"{st.session_state.logic['name']} has now been added (this is when the strat are added to sql)"
+    if st.button("ok"):
+        st.rerun()
