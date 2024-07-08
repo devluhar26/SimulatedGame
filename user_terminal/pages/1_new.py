7233c3cf77cb5e96b782f648794c332da69965e9
dev <devluhar26@gmail.com>
2024-07-08 10:58:38 +0100

    .

--- user_terminal/pages/1_new.py
+++ user_terminal/pages/1_new.py
@@ -19,22 +19,11 @@ import json
 import streamlit as st
 from code_editor import code_editor
 
-with open('streamlit-code-editor/examples/resources/example_custom_buttons_bar_alt.json') as json_button_file_alt:
-    custom_buttons_alt = json.load(json_button_file_alt)
 
-# Load Info bar CSS from JSON file
-with open('streamlit-code-editor/examples/resources/example_info_bar.json') as json_info_file:
-    info_bar = json.load(json_info_file)
 
-# Load Code Editor CSS from file
-with open('streamlit-code-editor/examples/resources/example_code_editor_css.scss') as css_file:
-    css_text = css_file.read()
-
-with open('streamlit-code-editor/examples/resources/example_python_code.py') as python_file:
-    demo_sample_python_code = python_file.read()
+demo_sample_python_code = ""
 
 # construct component props dictionary (->Code Editor)
-comp_props = {"css": css_text, "globalCSS": ":root {\n  --streamlit-dark-font-family: monospace;\n}"}
 
 mode_list = ["abap", "abc", "actionscript", "ada", "alda", "apache_conf", "apex", "applescript", "aql", "asciidoc",
              "asl", "assembly_x86", "autohotkey", "batchfile", "bibtex", "c9search", "c_cpp", "cirru", "clojure",
@@ -78,7 +67,6 @@ theme = "default"
 shortcuts = "vscode"
 focus = False
 wrap = True
-btns = custom_buttons_alt
 
 st.markdown(
     '<h1><a href="https://github.com/bouzidanas/streamlit.io/tree/master/streamlit-code-editor">Streamlit Code Editor</a> Demo</h1>',
@@ -104,32 +92,12 @@ with st.expander("Settings", expanded=True):
     focus = col_c.checkbox("focus", False)
     wrap = col_cb.checkbox("wrap", True)
 
-with st.expander("Components"):
-    c_buttons = st.checkbox("custom buttons (JSON)", False)
-    if c_buttons:
-        response_dict_btns = code_editor(json.dumps(custom_buttons_alt, indent=2), lang="json", height=8,
-                                         buttons=btn_settings_editor_btns)
-
-        if response_dict_btns['type'] == "submit" and len(response_dict_btns['text']) != 0:
-            btns = json.loads(response_dict_btns['text'])
-    else:
-        btns = []
-
-    i_bar = st.checkbox("info bar (JSON)", False)
-    if i_bar:
-        response_dict_info = code_editor(json.dumps(info_bar, indent=2), lang="json", height=8,
-                                         buttons=btn_settings_editor_btns)
-
-        if response_dict_info['type'] == "submit" and len(response_dict_info['text']) != 0:
-            info_bar = json.loads(response_dict_info['text'])
-    else:
-        info_bar = {}
 
 st.write("### Output:")
 # construct props dictionary (->Ace Editor)
 ace_props = {"style": {"borderRadius": "0px 0px 8px 8px"}}
 response_dict = code_editor(demo_sample_python_code, height=height, lang=language, theme=theme, shortcuts=shortcuts,
-                            focus=focus, buttons=btns, info=info_bar, props=ace_props, response_mode="debounce",
+                            focus=focus , props=ace_props, response_mode="debounce",
                             options={"wrap": wrap})
 
 if response_dict['type'] != "" and len(response_dict['id']) != 0:
