c56d407a1503c1f1a9394735f6f757ec3d1617b5
dev <devluhar26@gmail.com>
2024-07-08 16:51:44 +0100

    .

--- user_terminal/pages/1_new.py
+++ user_terminal/pages/1_new.py
@@ -46,15 +46,16 @@ ace_props = {"style": {"borderRadius": "0px 0px 8px 8px"}}
 response_dict = code_editor("", height=height,   buttons=btns, info=info_bar, props=ace_props)
 
 @st.experimental_dialog("Create a new trading strategy")
-def logic(name):
+def logic(name,code):
     st.write(f"set the trading logic for {name}")
     ##add bot logic widgets here
     if st.button("add"):
-        repo.create_file(str(random.randint(0, 5000)) + ".py", "it works", response_dict['text'], branch="main", )
+        print(response_dict['text'])
+        repo.create_file(str(random.randint(0, 5000)) + ".py", "it works", code, branch="main", )
 
         st.rerun()
 
 #####
 if st.button("impliment"):
-    logic(name)
+    logic(name,response_dict['text'])
 
