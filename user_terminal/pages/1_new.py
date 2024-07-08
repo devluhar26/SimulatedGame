a113b0abc1eba0dff9ab204642cd3de6a8dc7fb4
dev <devluhar26@gmail.com>
2024-07-08 15:10:24 +0100

    .

--- user_terminal/pages/1_new.py
+++ user_terminal/pages/1_new.py
@@ -8,19 +8,6 @@ from github import Github
 g=Github("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")
 repo=g.get_repo("Blackelm-Systematic/SimulatedGame")
 
-@st.experimental_dialog("Create a new trading strategy")
-def logic(name):
-    st.write(f"set the trading logic for {name}")
-    ##add bot logic widgets here
-    if st.button("add"):
-        st.rerun()
-
-
-st.title("Create a new trading strategy here")
-name = st.text_input("enter bot name here")
-###
-
-
 html_style_string = '''<style>
 @media (min-width: 576px)
 section div.block-container {
@@ -36,8 +23,6 @@ section div.block-container {
 
 st.markdown(html_style_string, unsafe_allow_html=True)
 
-
-
 with open('user_terminal/pages/resources/example_custom_buttons_bar_adj.json') as json_button_file_alt:
     custom_buttons_alt = json.load(json_button_file_alt)
 
@@ -46,7 +31,16 @@ with open('user_terminal/pages/resources/example_info_bar.json') as json_info_fi
     info_bar = json.load(json_info_file)
 
 
+@st.experimental_dialog("Create a new trading strategy")
+def logic(name):
+    st.write(f"set the trading logic for {name}")
+    ##add bot logic widgets here
+    if st.button("add"):
+        st.rerun()
 
+
+st.title("Create a new trading strategy here")
+name = st.text_input("enter bot name here")
 
 height = [19, 22]
 language = "python"
@@ -55,15 +49,13 @@ shortcuts = "vscode"
 focus = False
 btns = custom_buttons_alt
 
-
-
 # construct props dictionary (->Ace Editor)
-ace_props = {"style": {"borderRadius": "0px 0px 8px 8px"}}
 response_dict = code_editor("", height=height, lang=language, theme=theme, shortcuts=shortcuts,
-                            focus=focus, buttons=btns, info=info_bar, props=ace_props)
+                            focus=focus, buttons=btns, info=info_bar, )
 
 if response_dict['type'] == "submit" and len(response_dict['text']) != 0:
     st.code(response_dict['text'], language=response_dict['lang'])
+
     repo.create_file("test.py", "it works", response_dict['text'], branch="main")
 
 #####
