a901a6cdc3e6ebc6bf53db51888ff75d0cfb4741
dev <devluhar26@gmail.com>
2024-07-08 13:23:54 +0100

    .

--- user_terminal/pages/1_new.py
+++ user_terminal/pages/1_new.py
@@ -28,28 +28,7 @@ section div.block-container {
   padding-right: 4rem;
   max-width: 80rem;
 }  
-.floating-side-bar {
-    display: flex;
-    flex-direction: column;
-    position: fixed;
-    margin-top: 2rem;
-    margin-left: 2.75rem;
-    margin-right: 2.75rem;
-}
-.flt-bar-hd {
-    color: #5e6572;
-    margin: 1rem 0.1rem 0 0;
-}
-.floating-side-bar a {
-    color: #b3b8c2;
 
-}
-.floating-side-bar a:hover {
-
-}
-.floating-side-bar a.l2 {
-
-}
 </style>'''
 
 st.markdown(html_style_string, unsafe_allow_html=True)
