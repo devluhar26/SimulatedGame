98a4a266b3a4e1354a542b1e3b05cfffa0b14eb8
dev <devluhar26@gmail.com>
2024-07-08 10:32:46 +0100

    .

--- user_terminal/pages/1_new.py
+++ user_terminal/pages/1_new.py
@@ -24,4 +24,4 @@ if "logic" not in st.session_state:
 else:
     f"{st.session_state.logic['name']} has now been added (this is when the strat are added to sql)"
     if st.button("ok"):
-        st.rerun()
+        st.write(st.session_state.logic)
