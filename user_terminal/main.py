244d474fe1fc583460bd30052559b74b3eb72348
dev <devluhar26@gmail.com>
2024-07-09 11:05:46 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -30,7 +30,7 @@ with tab1:
     chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
     st.line_chart(chart_data)
-
+    st.write("add portfolio value here")
 
 
 with tab2:
