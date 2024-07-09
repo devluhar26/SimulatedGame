fd871229880f5507f7ef79d82c1d3ad071ad54d3
dev <devluhar26@gmail.com>
2024-07-09 21:45:33 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -1,27 +1,49 @@
+import sqlite3
+
 import streamlit as st
 
 if "role" not in st.session_state:
     st.session_state.role = None
 
+connect_credentials = sqlite3.connect( "credentials.db" )
 
+curs_credentials = connect_credentials.cursor()
+# used to store all the usernames and passwords as a 2d array
+credentials = []
+def retrieve_credentials():             #STATIC METHOD
+    curs_credentials.execute( "SELECT username,password FROM Credentials" )
 
+    for data in curs_credentials.fetchall():
+        temp = []  # creates 2d array for all credentials
+        for x in data:
+            temp.append( x )
+        credentials.append( temp )  #3D array
+print(credentials)
+def checker(username,password):
+    retrieve_credentials()
+    temp=[username,password]
+    if temp in credentials:
+        st.session_state.user = username
+        st.rerun()
 
 def login():
 
     st.header("Log in")
-    roley = st.text_input("enter username")
+    username = st.text_input("enter username")
+    password = st.text_input("enter password")
 
     if st.button("Log in"):
-        st.session_state.role = roley
-        st.rerun()
+        ##add a login checking system here
+
+
 
 
 def logout():
-    st.session_state.role = None
+    st.session_state.user = None
     st.rerun()
 
 
-role = st.session_state.role
+role = st.session_state.user
 
 logout_page = st.Page(logout, title="Log out")
 
@@ -39,7 +61,7 @@ request_2 = st.Page(
 account_pages = [logout_page]
 request_pages = [request_1, request_2]
 
-st.title("Welcome "+st.session_state.role)
+st.title("Welcome "+st.session_state.user)
 
 if st.session_state.role != None:
     pg = st.navigation({"Account": account_pages} | {"Tools": [request_1, request_2]})
