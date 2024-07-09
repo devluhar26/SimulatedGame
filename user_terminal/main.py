2807b3ffb96f1812faf9f9a7326736c868c3de0c
dev <devluhar26@gmail.com>
2024-07-09 22:46:58 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -63,7 +63,7 @@ request_2 = st.Page(
 account_pages = [logout_page]
 request_pages = [request_1, request_2]
 
-st.title("Welcome "+str(st.session_state.user))
+st.title("Blackelm")
 
 if st.session_state.user != None:
     pg = st.navigation({"Account": account_pages} | {"Tools": [request_1, request_2]})
