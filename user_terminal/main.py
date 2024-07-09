5b627e0c5b2c3fc01387df9d9707932d07f5c8fd
dev <devluhar26@gmail.com>
2024-07-09 20:38:57 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -46,9 +46,9 @@ if st.session_state.role in ["Requester"]:
 
 
 
-if len(page_dict) > 0:
-    pg = st.navigation({"Account": account_pages} | page_dict)
-else:
-    pg = st.navigation([st.Page(login)])
-
-pg.run()
+# if len(page_dict) > 0:
+#     pg = st.navigation({"Account": account_pages} | page_dict)
+# else:
+#     pg = st.navigation([st.Page(login)])
+#
+# pg.run()
