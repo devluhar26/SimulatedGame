d0f0c1ba38a4ebce95740112b4d70751dd0e3995
dev <devluhar26@gmail.com>
2024-07-09 20:42:36 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -41,6 +41,7 @@ request_pages = [request_1, request_2]
 st.title("Request manager")
 
 page_dict = {}
+st.write(st.session_state)
 if st.session_state.role in ["Requester"]:
     page_dict["Request"] = request_pages
 
