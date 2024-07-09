4aa301ec771cf8e52b986a93af7d0fc10eb7c413
dev <devluhar26@gmail.com>
2024-07-09 14:31:38 +0100

    .

--- user_terminal/pages/1_new.py
+++ user_terminal/pages/1_new.py
@@ -25,38 +25,39 @@ section div.block-container {
 </style>'''
 
 st.markdown(html_style_string, unsafe_allow_html=True)
-
-@st.experimental_dialog("Create a new trading strategy")
-def logic(name,code):
-    st.write(f"set the trading logic for {name}")
-    ##add bot logic widgets here
-    if st.button("add"):
-        repo.create_file(str(random.randint(0, 5000)) + ".py", "it works", code, branch="main", )
-        st.rerun()
-
-
-st.title("Create a new trading strategy here")
-name = st.text_input("enter bot name here")
-###
-
-with open('user_terminal/pages/resources/example_custom_buttons_bar_adj.json') as json_button_file_alt:
-    custom_buttons_alt = json.load(json_button_file_alt)
-
-with open('user_terminal/pages/resources/example_info_bar.json') as json_info_file:
-    info_bar = json.load(json_info_file)
-
-height = [20, 22]
-btns = custom_buttons_alt
-st.write("Program your strategy below then Hit Save")
+if st.session_state.login==False:
+    login()
+else:
+    @st.experimental_dialog("Create a new trading strategy")
+    def logic(name,code):
+        st.write(f"set the trading logic for {name}")
+        ##add bot logic widgets here
+        if st.button("add"):
+            repo.create_file(str(random.randint(0, 5000)) + ".py", "it works", code, branch="main", )
+            st.rerun()
 
 
-response_dict = code_editor("", height=height,   buttons=btns, info=info_bar)
-if response_dict['type'] == "submit" and len(response_dict['text']) != 0:
-    code=response_dict['text']
-    logic(name,code)
-elif  response_dict['type'] == "submit" and len(response_dict['text']) == 0:
-    st.warning('Add your strategy before Hitting Save', icon="⚠️")
+    st.title("Create a new trading strategy here")
+    name = st.text_input("enter bot name here")
+    ###
 
-#####
-st.write(st.session_state)
+    with open('user_terminal/pages/resources/example_custom_buttons_bar_adj.json') as json_button_file_alt:
+        custom_buttons_alt = json.load(json_button_file_alt)
+
+    with open('user_terminal/pages/resources/example_info_bar.json') as json_info_file:
+        info_bar = json.load(json_info_file)
+
+    height = [20, 22]
+    btns = custom_buttons_alt
+    st.write("Program your strategy below then Hit Save")
+
+
+    response_dict = code_editor("", height=height,   buttons=btns, info=info_bar)
+    if response_dict['type'] == "submit" and len(response_dict['text']) != 0:
+        code=response_dict['text']
+        logic(name,code)
+    elif  response_dict['type'] == "submit" and len(response_dict['text']) == 0:
+        st.warning('Add your strategy before Hitting Save', icon="⚠️")
+
+    #####
 
