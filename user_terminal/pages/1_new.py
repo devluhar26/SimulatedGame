d71611e985006c2b78b2eaa8235b4241cd9514cb
dev <devluhar26@gmail.com>
2024-07-09 14:37:51 +0100

    .

--- user_terminal/pages/1_new.py
+++ user_terminal/pages/1_new.py
@@ -28,6 +28,11 @@ st.markdown(html_style_string, unsafe_allow_html=True)
 if "login" not in st.session_state:
     st.session_state.login=False
 
+@st.experimental_dialog("login",width="large")
+def login():
+    if st.button("login"):
+        st.session_state.login=True
+        st.rerun()
 if st.session_state.login==False:
     login()
 else:
