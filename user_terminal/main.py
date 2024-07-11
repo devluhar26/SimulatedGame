5e7381e5e68c92fd8e66130c27bd8bb645d804ed
dev <devluhar26@gmail.com>
2024-07-11 11:37:09 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -28,6 +28,9 @@ def add_credentials(username,password):
     curs_credentials.execute("INSERT INTO  Credentials (username,password) VALUES (?,?)",
                              (username,password))
     connect_credentials.commit()
+    connect_credentials.close()
+    retrieve_credentials()
+    st.write(credentials)
     st.success("you have registered")
 def checker(username,password):
     retrieve_credentials()
