c0a69ae76bf461195545b19780fdae72beecfba7
dev <devluhar26@gmail.com>
2024-07-11 11:52:02 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -1,42 +1,40 @@
 import sqlite3
+import os.path
 
 import streamlit as st
 st.set_page_config(layout='wide')
 
 if "user" not in st.session_state:
     st.session_state.user = None
-import os.path
-def open_SQL():
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    db_path = os.path.join(BASE_DIR, "credentials.db")
-    connect_credentials= sqlite3.connect(db_path)
-# used to store all the usernames and passwords as a 2d array
-open_SQL()
-curs_credentials = open_SQL.connect_credentials.cursor()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "credentials.db")
+connect_credentials= sqlite3.connect(db_path)
+curs_credentials = connect_credentials.cursor()
+
+# used to store all the usernames and passwords as a 2d array
 credentials = []
 def retrieve_credentials():             #STATIC METHOD
-    open_SQL.curs_credentials.execute( "SELECT * FROM Credentials" )
-    for data in open_SQL.curs_credentials.fetchall():
+    curs_credentials.execute( "SELECT * FROM Credentials" )
+    for data in curs_credentials.fetchall():
         temp = []  # creates 2d array for all credentials
         for x in data:
             temp.append( x )
-        open_SQL.credentials.append( temp )  #3D array
+        credentials.append( temp )  #3D array
 
 def add_credentials(username,password):
 
-    open_SQL.curs_credentials.execute("INSERT INTO  Credentials (username,password) VALUES (?,?)",
+    curs_credentials.execute("INSERT INTO  Credentials (username,password) VALUES (?,?)",
                              (username,password))
-    open_SQL.connect_credentials.commit()
-    open_SQL.connect_credentials.close()
+    connect_credentials.commit()
     retrieve_credentials()
-    st.write(open_SQL.credentials)
+    st.write(credentials)
     st.success("you have registered")
 def checker(username,password):
     retrieve_credentials()
     temp=[str(username),str(password)]
     ##add some more errors ie no input and try registering
-    if temp in open_SQL.credentials:
+    if temp in credentials:
         st.session_state.user = username
         st.rerun()
     else:
@@ -55,14 +53,14 @@ def login():
     with col2:
         if st.button("Register", use_container_width=True):
             retrieve_credentials()
-            if username in [row[0] for row in open_SQL.credentials]:
+            if username in [row[0] for row in credentials]:
                 st.warning("this username already exist, try a different one")
                 return
             if username == "" or password == "":
                 st.warning("one or more of the fields are blank, please add some text")
             else:
                 add_credentials(username,password)
-
+                st.rerun()
 
         ##add a login checking system here
 
