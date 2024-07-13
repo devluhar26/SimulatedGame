723d4dc9f6518a7702c4d65554aaf39ff43acb38
dev <devluhar26@gmail.com>
2024-07-13 09:36:34 +0100

    .

--- user_terminal/main.py
+++ user_terminal/main.py
@@ -11,7 +11,7 @@ import os.path
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR,"credentials.db")
-connect_credentials = sqlite3.connect( db_path )
+connect_credentials = sqlite3.connect(db_path)
 
 curs_credentials = connect_credentials.cursor()
 #Devs personal access token, need to change it
