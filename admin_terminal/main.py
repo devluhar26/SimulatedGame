496ea03fc5014883b38d32019492120f5cd1a5ae
dev <devluhar26@gmail.com>
2024-07-15 11:30:17 +0100

    .

--- admin_terminal/main.py
+++ admin_terminal/main.py
@@ -1,23 +1,6 @@
 import streamlit as st
+from streamlit_elements import elements, mui, html
 
 st.set_page_config(layout='wide')
 
-row1 = st.columns(3)
-row2 = st.columns(3)
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
-for col in row1 + row2:
-    tile = col.container(height=400)
-    tile.title(":balloon:")
-
-    with tab1:
-        st.header("A cat")
-        st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
-    with tab2:
-        st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-    with tab3:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
