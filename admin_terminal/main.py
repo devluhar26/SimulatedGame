213b6490cc4b0e4fba250d43a15a906699161b12
dev <devluhar26@gmail.com>
2024-07-15 11:24:53 +0100

    .

---
+++ admin_terminal/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+row1 = st.columns(3)
+row2 = st.columns(3)
+
+for col in row1 + row2:
+    tile = col.container(height=120)
+    tile.title(":balloon:")
