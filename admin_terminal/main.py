cf25c13fd0d32ffbfbc10a2b6e8087b232f65915
dev <devluhar26@gmail.com>
2024-07-15 11:50:41 +0100

    .

--- admin_terminal/main.py
+++ admin_terminal/main.py
@@ -6,28 +6,10 @@ from streamlit import session_state as state
 from streamlit_elements import elements, sync, event
 from types import SimpleNamespace
 
-from .dashboard import Dashboard, Editor, Card, DataGrid, Radar, Pie, Player
 
 
 def main():
-    st.write(
-        """
-        ✨ Streamlit Elements &nbsp; [![GitHub][github_badge]][github_link] [![PyPI][pypi_badge]][pypi_link]
-        =====================
 
-        Create a draggable and resizable dashboard in Streamlit, featuring Material UI widgets,
-        Monaco editor (Visual Studio Code), Nivo charts, and more!
-
-        [github_badge]: https://badgen.net/badge/icon/GitHub?icon=github&color=black&label
-        [github_link]: https://github.com/okld/streamlit-elements
-
-        [pypi_badge]: https://badgen.net/pypi/v/streamlit-elements?icon=pypi&color=black&label
-        [pypi_link]: https://pypi.org/project/streamlit-elements
-        """
-    )
-
-    with st.expander("GETTING STARTED"):
-        st.write((Path(__file__).parent/"README.md").read_text())
 
     st.title("")
 
