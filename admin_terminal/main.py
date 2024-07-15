2caaa067d8253b2ef28cdb7147c01697b57a4108
dev <devluhar26@gmail.com>
2024-07-15 12:08:45 +0100

    .

--- admin_terminal/main.py
+++ admin_terminal/main.py
@@ -5,14 +5,14 @@ row1col1,row1col2 = st.columns([2,3])
 row2col1,row2col2 = st.columns([2,3])
 
 with row1col1:
-    tile11 = row1col1.container(height=120)
+    tile11 = row1col1.container(height=320)
     tile11.title("11")
 with row1col2:
-    tile12 = row1col2.container(height=120)
+    tile12 = row1col2.container(height=320)
     tile12.title("12")
 with row2col1:
-    tile21 = row2col1.container(height=120)
+    tile21 = row2col1.container(height=320)
     tile21.title("21")
 with row2col2:
-    tile22 = row2col2.container(height=120)
+    tile22 = row2col2.container(height=320)
     tile22.title("22")
