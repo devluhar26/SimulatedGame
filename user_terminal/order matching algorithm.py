50ca47bb8e11913b92602abdb0ca556de05c2286
dev <devluhar26@gmail.com>
2024-07-16 15:28:46 +0100

    .

--- user_terminal/order matching algorithm.py
+++ user_terminal/order matching algorithm.py
@@ -5,9 +5,12 @@ curs_exchange = connect_exchange.cursor()
 
 
 def execute_order(username,buy_sell,pps,quantity,stock):
-    order=open("ordernum.txt","r")
-    ordernum=order.readline()
-    print(ordernum)
+
+    ordernum=int(open("ordernum.txt","r").readline())
+    new=open("ordernum.txt", "w")
+    new.write(str(ordernum+1))
+    new.close()
+
     #curs_exchange.execute("INSERT INTO  active orders (order number,buy or sell, username, ask bid price per share,quantity, stock, time of execution) VALUES (?,?,?,?,?,?,?)",
     #                         (username, password))
 execute_order(1,1,1,1,1)
