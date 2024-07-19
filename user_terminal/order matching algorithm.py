4bbd86ddb8c9e27488b93efa6591541110ca7f53
dev <devluhar26@gmail.com>
2024-07-19 10:14:08 +0100

    .

--- user_terminal/order matching algorithm.py
+++ user_terminal/order matching algorithm.py
@@ -1,16 +1,26 @@
 import sqlite3
 import time
+from time import gmtime, strftime
+
 connect_exchange = sqlite3.connect( "exchange.db" )
 curs_exchange = connect_exchange.cursor()
 
 
 def execute_order(username,buy_sell,pps,quantity,stock):
-
     ordernum=int(open("ordernum.txt","r").readline())
-    new=open("ordernum.txt", "w")
-    new.write(str(ordernum+1))
-    new.close()
+    curs_exchange.execute("INSERT INTO  active orders (order number,buy or sell, username, ask bid price per share,quantity, stock, time of execution) VALUES (?,?,?,?,?,?,?)",
+                             (ordernum,username,buy_sell,pps,quantity, stock ,time.strftime("%a, %d %b %Y %I:%M:%S %p %Z", time.gmtime())))
 
-    #curs_exchange.execute("INSERT INTO  active orders (order number,buy or sell, username, ask bid price per share,quantity, stock, time of execution) VALUES (?,?,?,?,?,?,?)",
-    #                         (username, password))
-execute_order(1,1,1,1,1)
+    new = open("ordernum.txt", "w")
+    new.write(str(ordernum + 1))
+    new.close()
+    check_database(username,buy_sell,pps,quantity,stock)
+
+def check_database(username,buy_sell,pps,quantity,stock):
+    curs_exchange("SELECT * FROM active orders WHERE (stock==stock) and (username!=username) and (buy or sell!= buy_sell) and  (ask bid price per share<=pps) and quantity>=quantity ORDERBY and min(abs(ask bid price per share-pps)) min(order number) (?,?)"(stock,username))
+    if curs_exchange.fetchall()==None:
+        return
+    else:
+        trade_to_execute=curs_exchange.fetchall()[0]
+
+
