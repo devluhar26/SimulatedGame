e3433cde13c88e081b5ab51d196d90707fc07738
dev <devluhar26@gmail.com>
2024-07-22 12:40:49 +0100

    .

--- user_terminal/order matching algorithm.py
+++ user_terminal/order matching algorithm.py
@@ -8,26 +8,34 @@ curs_exchange = connect_exchange.cursor()
 
 def execute_order(username,buy_sell,pps,quantity,stock):
     ordernum=int(open("ordernum.txt","r").readline())
-    curs_exchange.execute("INSERT INTO  active orders (order number,buy or sell, username, ask bid price per share,quantity, stock, time of execution) VALUES (?,?,?,?,?,?,?)",
-                             (ordernum,username,buy_sell,pps,quantity, stock ,time.strftime("%a, %d %b %Y %I:%M:%S %p %Z", time.gmtime())))
-
+    curs_exchange.execute("INSERT INTO  active_orders (order_number,buy_or_sell, username, ask_bid_price_per_share,quantity, stock, time_of_execution) VALUES (?,?,?,?,?,?,?)",
+                             (ordernum,username,buy_sell,pps,quantity, stock ,time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())))
+    #adjust[
+    connect_exchange.commit()
     new = open("ordernum.txt", "w")
     new.write(str(ordernum + 1))
     new.close()
-    check_database(username,buy_sell,pps,quantity,stock)
+    #check_database(username,buy_sell,pps,quantity,stock)
+execute_order("dev","sell","105","10","AAPL")
 
-def check_database(username,buy_sell,pps,quantity,stock):
-    #seperate out buy from sell, ive added it to the sql query below but that is incorrect and needs removing
-    if buy_sell=="buy":
-        curs_exchange("SELECT * FROM active orders WHERE (stock==stock) and (username!=username) and  (ask bid price per share<=pps) and quantity>=quantity ORDERBY and min(abs(ask bid price per share-pps)) min(order number) (?,?)"(stock,username))
-        if curs_exchange.fetchall()==None:
-            return
-        else:
-            trade_to_execute=curs_exchange.fetchall()[0]
-            #now the quantity algorithm needs to be sorted
-            #fetch quantity from the trade extracted and apply things in flowchart
-
-
-def quantity_adjustments(username,buy_sell,pps,quantity,stock,trade_to_execute):
-    if buy_sell=="buy":
-        pass
+# def check_database(username,buy_sell,pps,quantity,stock):
+#     if buy_sell=="buy":
+#         curs_exchange("SELECT * FROM active orders WHERE (stock==stock) and (username!=username) and  (ask bid price per share<=pps) and quantity>=quantity ORDERBY and min(abs(ask bid price per share-pps)) min(order number) (?,?)"(stock,username))
+#         if curs_exchange.fetchall()==None:
+#             return
+#         else:
+#             trade_to_execute=curs_exchange.fetchall()[0]
+#             #now the quantity algorithm needs to be sorted
+#             #fetch quantity from the trade extracted and apply things in flowchart
+#
+#
+# def quantity_adjustments(username,buy_sell,pps,quantity,stock,trade_to_execute):
+#     if buy_sell=="buy":
+#         if trade_to_execute[4]<quantity:
+#             #add sellers quantity of stock to buyers portfolio then remove sellers stock from sellers portfolio, modify buyers active order by buyer quantity- sellers quantity
+#             pass
+#         elif trade_to_execute[4]>quantity:
+#             #add buyers quantity of stock to buyers portfolio then remove buyer from active orders and keep seller on active order with reduced quantity
+#             pass
+#         else:
+#            pass
--- user_terminal/read stock price.py
+++ user_terminal/read stock price.py
@@ -1,24 +1,42 @@
 from github import Github
 import sqlite3
 
-connect_stock = sqlite3.connect( "stock.db" )
+connect_stock = sqlite3.connect( "stock_prices.db" )
 curs_stock = connect_stock.cursor()
 
 
 #gets the current stock price for stock A
-def current_stockprice(stock):
-    curs_stock.execute("SELECT stockprice FROM STOCK A WHERE max(time)")####
-    stock_price=curs_stock.fetchone()
-    return stock_price
+def current_bid_price(stock):
+    curs_stock.execute("SELECT bid FROM ? WHERE time = (SELECT MAX(time) FROM stock A)",(stock))####
+    bid_price=curs_stock.fetchone()
+    return bid_price
 
 #gets an array of stock prices for stock A from start datetime to end date time
-def stockprice_inveval(stock, start, end):
-    curs_stock.execute("SELECT stockprice FROM STOCK A WHERE ")#####
-    stock_price_raw = curs_stock.fetchall()
-    stock_price=[]
-    for data in  stock_price_raw:
+def bid_price_inveval(stock, start, end):
+    curs_stock.execute("SELECT bid FROM ? WHERE time BETWEEN ? AND ?",(stock,start,end))#####
+    bid_price_raw = curs_stock.fetchall()
+    bid_price=[]
+    for data in  bid_price_raw:
         temp = []  # creates 2d array per row
         for x in data:
             temp.append( x )
-        stock_price.append( temp )  #3D array
-    return stock_price
+        bid_price.append( temp )  #3D array
+    return bid_price
+
+#gets the current stock price for stock A
+def current_ask_price(stock):
+    curs_stock.execute("SELECT ask FROM ? WHERE time = (SELECT MAX(time) FROM stock A)",(stock))####
+    ask_price=curs_stock.fetchone()
+    return ask_price
+
+#gets an array of stock prices for stock A from start datetime to end date time
+def ask_price_inveval(stock, start, end):
+    curs_stock.execute("SELECT ask FROM ? WHERE time BETWEEN ? AND ?",(stock,start,end))#####
+    ask_price_raw = curs_stock.fetchall()
+    ask_price=[]
+    for data in  ask_price_raw:
+        temp = []  # creates 2d array per row
+        for x in data:
+            temp.append( x )
+        ask_price.append( temp )  #3D array
+    return ask_price
