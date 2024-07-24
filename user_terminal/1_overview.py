4884f45dd0feb8dc3cdf652f3b64d2804192ccdd
dev <devluhar26@gmail.com>
2024-07-24 15:06:10 +0100

    .

--- user_terminal/1_overview.py
+++ user_terminal/1_overview.py
@@ -164,7 +164,7 @@ with tab3:
     if st.button("add"):
         repo.update_file("user_terminal/"+ option + ".py", "it works", response_dict['text'], branch="main",sha=repo.get_contents("user_terminal/"+option+".py",ref="main").sha )
 
-        curs_user.execute("UPDATE strategy SET (stock, take_profit,stop_loss,min_size,max_size,min_timeframe,max_timeframe,trade_frequency) = (?,?,?,?,?,?,?,?,?,?) WHERE (strategy_name)=(?)",(stock,take_profit,stop_loss,min_size,max_size,min_timeframe,max_timeframe,trades_per_hour,option))
+        curs_user.execute("UPDATE strategy SET (stock, take_profit,stop_loss,min_size,max_size,min_timeframe,max_timeframe,trade_frequency) = (?,?,?,?,?,?,?,?) WHERE (strategy_name)=(?)",(stock,take_profit,stop_loss,min_size,max_size,min_timeframe,max_timeframe,trades_per_hour,option))
         connect_user.commit()
         file = open(user_db_path, "rb")
         repo.update_file(local_path, ".", file.read(), repo.get_contents(local_path).sha, "main")
