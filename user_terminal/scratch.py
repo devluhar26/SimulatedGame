098da409a94be4aa4f8b107eda5f6895e0fd111b
dev <devluhar26@gmail.com>
2024-07-24 14:31:45 +0100

    .

--- user_terminal/scratch.py
+++ user_terminal/scratch.py
@@ -1,12 +1,10 @@
+import os
 import sqlite3
-def tuple_to_array(tuple):
-    array=[]
-    for data in  tuple:
-        temp = []  # creates 2d array for all credentials
-        for x in data:
-            temp.append( x )
-        array.append( temp )  #3D array
-    return array
-conn_stock = sqlite3.connect("stock_prices.db")
-curs_stock = conn_stock.cursor()
-print(*[row[0] for row in curs_stock.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()])
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+user_db_path = os.path.join(BASE_DIR, "bob" + ".db")
+connect_user = sqlite3.connect(user_db_path)
+curs_user = connect_user.cursor()
+
+print(str(curs_user.execute("SELECT strategy_location FROM strategy WHERE strategy_name=?",("dsftryr",)).fetchone()[0]))
+print(open(os.path.join(BASE_DIR,str(curs_user.execute("SELECT strategy_location FROM strategy WHERE strategy_name=?",("dsftryr",)).fetchone()[0])),"r").read())
