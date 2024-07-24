8579b6ba0659aaa9392db0f97acd316d0efa2410
dev <devluhar26@gmail.com>
2024-07-24 12:30:53 +0100

    .

--- user_terminal/2_new.py
+++ user_terminal/2_new.py
@@ -41,22 +41,13 @@ if "bot_name" not in st.session_state:
 
 @st.experimental_dialog("Create a new trading strategy")
 def logic(name,code):
-
     st.write(f"set the trading logic for {name}")
-    st.write(stock_name)
-    values = st.slider(
-        "Select a range of values",
-        0.0, 100.0, (25.0, 75.0))
-    options = st.multiselect("Select the stocks you wish to apply the strategy to",stock_name)
-    st.write("You selected:", options)
-    number = st.number_input("Insert a number")
+    options = st.selectbox("Select the stocks you wish to apply the strategy to",stock_name)
     local_path = "user_terminal/"+ st.session_state.user + ".db"
     ##add bot logic widgets here
     if st.button("add"):
         repo.create_file("user_terminal/"+ st.session_state.bot_name + ".py", "it works", code, branch="main", )
-
         curs_user.execute("INSERT INTO strategy(strategy_name, strategy_location,stock, take_profit,stop_loss,min_size,max_size,timeframe,trade_frequency) VALUES (?,?,?,?,?,?,?,?,?,?)",(st.session_state.bot_name,"user_terminal/"+ st.session_state.bot_name + ".py",))
-
         connect_user.commit()
         file = open(user_db_path, "rb")
         repo.update_file(local_path, ".", file.read(), repo.get_contents(local_path).sha, "main")
