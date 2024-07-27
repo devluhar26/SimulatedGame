984bf0acfd8bc6b0ada98919454e839750efb90c
dev <devluhar26@gmail.com>
2024-07-27 14:21:54 +0100

    .

--- user_terminal/scratch.py
+++ user_terminal/scratch.py
@@ -1,10 +1,19 @@
 import os
+import random
 import sqlite3
+import time
+from time import gmtime, strftime
+from user_terminal.order_matching_algorithm import curs_exchange, connect_exchange
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+for x in range(100):
+    ordernum = int(open("ordernum.txt", "r").readline())
+    curs_exchange.execute(
+        "INSERT INTO  active_orders (order_number,buy_or_sell, username, ask_bid_price_per_share,quantity, stock, time_of_execution) VALUES (?,?,?,?,?,?,?)",
+        (ordernum, "buy", "dev", random.uniform(60,120), random.uniform(10,1000), "AAPL", time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())))
 
-user_db_path = os.path.join(BASE_DIR, "bob" + ".db")
-connect_user = sqlite3.connect(user_db_path)
-curs_user = connect_user.cursor()
-
-print(str(curs_user.execute("SELECT strategy_location FROM strategy WHERE strategy_name=?",("dsftryr",)).fetchone()[0]))
-print(open(os.path.join(BASE_DIR,str(curs_user.execute("SELECT strategy_location FROM strategy WHERE strategy_name=?",("dsftryr",)).fetchone()[0])),"r").read())
+    print(x)
+    connect_exchange.commit()
+    new = open("ordernum.txt", "w")
+    new.write(str(ordernum + 1))
+    new.close()
