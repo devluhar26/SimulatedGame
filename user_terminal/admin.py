f669806e16e98fbc0af0f3295c3a7b3cdc9e4f3d
dev <devluhar26@gmail.com>
2024-07-28 15:18:42 +0100

    .

--- user_terminal/admin.py
+++ user_terminal/admin.py
@@ -30,8 +30,9 @@ with row1col1:
         stock = tile11.selectbox("Select which stock you would like to use the strategy on",[row[0] for row in curs_stock.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()])
     except:
         stock="AAPL"
-    chart_data = pd.DataFrame(tuple_to_array(curs_stock.execute(f"SELECT * FROM ",(stock,)).fetchall()), columns=["bid","ask","last trade price","time"],)
-    tile11.line_chart(chart_data,height=590, use_container_width=True)
+
+    # chart_data = pd.DataFrame(tuple_to_array(curs_stock.execute(f"SELECT * FROM ",(stock[0],)).fetchall()), columns=["bid","ask","last trade price","time"],)
+    # tile11.line_chart(chart_data,height=590, use_container_width=True)
 
 
 with row1col2:
