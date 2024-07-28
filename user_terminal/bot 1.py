73709eb3a874505720e1489dc9970a1cecd90b23
dev <devluhar26@gmail.com>
2024-07-28 16:55:53 +0100

    .

--- user_terminal/bot 1.py
+++ user_terminal/bot 1.py
@@ -7,13 +7,13 @@ def main(username):
     names=get_stock_names()
     stock = names[random.randint(0, len(names) - 1)]
     choice=random.randint(0,1)
-    percent_adjust=round(random.uniform(-2,2)/100,4)
+    percent_adjust=round(random.uniform(0,2)/100,4)
     if choice==0:
         buy_sell="buy"
-        pps=round(current_bid_price(stock)*(1+percent_adjust),2)
+        pps=round(current_last_price(stock)*(1-percent_adjust),2)
     else:
         buy_sell="sell"
-        pps=round(current_ask_price(stock)*(1+percent_adjust),2)
+        pps=round(current_last_price(stock)*(1+percent_adjust),2)
     quantity=round(random.uniform(0.1, 0.5), 1)
     execute_order(username=username,buy_sell=buy_sell,pps=round(pps,2),quantity=quantity,stock=stock)
 
